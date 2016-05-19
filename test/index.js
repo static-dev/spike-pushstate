ef719c36ba9328eab659f30e0201670b6ca26b98
Jeff Escalante <hello@jenius.me>
2016-05-19 19:35:48 -0400

    basic test in place

--- test/index.js
+++ test/index.js
@@ -1,6 +1,23 @@
-const example = require('../lib/example')
 const test = require('ava')
+const path = require('path')
+const Spike = require('spike-core')
+const PushState = require('..')
 
-test('example exports correctly', (t) => {
-  t.is(example, 'wow es6!')
+const fixtures = path.join(__dirname, 'fixtures')
+
+test.cb('basic', (t) => {
+  const project = new Spike({
+    root: path.join(fixtures, 'basic'),
+    entry: { main: ['./main.js'] },
+    plugins: [new PushState()]
+  })
+
+  project.on('error', t.end)
+  project.on('warning', t.end)
+  project.on('compile', () => {
+    console.log('compiled!')
+    t.end()
+  })
+
+  project.compile()
 })
